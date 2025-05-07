7a3bafc5e0d1447a56fb1661e443bc4ffcf7dcbb
HottensiahNyanjui <hottensiahnyanjui@gmail.com>
2025-05-07 20:01:14 +0300

    Edited Output.tf

--- stage-1-Ansible-root/Stage_two/terraform/main.tf
+++ stage-1-Ansible-root/Stage_two/terraform/main.tf
@@ -9,3 +9,10 @@ resource "null_resource" "provision_vm" {
     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ../ansible/hosts ../ansible/playbook.yaml"
   }
 }
+
+output "ansible_inventory" {
+  value = <<EOT
+[web]
+yolo-ecommerce ansible_host=127.0.0.1 ansible_port=2222 ansible_user=vagrant ansible_ssh_private_key_file=${path.module}/../.vagrant/machines/default/virtualbox/private_key ansible_python_interpreter=/usr/bin/python3
+EOT
+}
