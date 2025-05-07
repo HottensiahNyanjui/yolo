1b96901687fc471bb086a4ec0af1def666422ea5
HottensiahNyanjui <hottensiahnyanjui@gmail.com>
2025-05-07 19:33:45 +0300

    Setup output.tf

---
+++ stage-1-Ansible-root/Stage_two/terraform/output.tf
@@ -0,0 +1,9 @@
+output "vagrant_connection_info" {
+  value = {
+    host  = "127.0.0.1"
+    port  = 2222
+    user  = "vagrant"
+    key   = "/home/hottensiah/DevOps/yolo/.vagrant/machines/default/virtualbox/private_key"
+    python_interpreter = "/usr/bin/python3"
+  }
+}
